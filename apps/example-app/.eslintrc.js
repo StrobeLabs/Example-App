429c815fe628f36f557e84c312c81e889fbbad9c
rdave8 <rohan8dave@gmail.com>
2024-07-19 17:12:43 -0400

    strobe -> strobelabs

--- apps/example-app/.eslintrc.js
+++ apps/example-app/.eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@strobe/eslint-config/next.js"],
+  extends: ["@strobelabs/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
