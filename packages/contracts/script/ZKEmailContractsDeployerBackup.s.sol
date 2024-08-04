75e7105299d333f3f88e8953bd2cd04a63135802
Gilad <freeslugs@gmail.com>
2024-08-03 23:43:24 -0400

    clean up deployers too

--- packages/contracts/script/ZKEmailContractsDeployerBackup.s.sol
+++ packages/contracts/script/ZKEmailContractsDeployerBackup.s.sol
@@ -4,26 +4,35 @@ pragma solidity ^0.8.25;
 import {Script} from "forge-std/src/Script.sol";
 import{ ProofTypeZKEmail} from "../src/zk-email/ProofTypeZKEmail.sol";
 import {console} from "forge-std/src/console.sol";
+// import {MyVerifier} from "../src/zk-email/MyVerifier.sol";
+import {ProofTypeGroth16} from "@strobelabs/contracts/src/proof-types/ProofTypeGroth16.sol";
 
-contract ZKEmailContractsDeployer is Script {
+
+contract ZKEmailContractsDeployerBackup is Script {
     function run() external {
         // Retrieve the private key from the environment variable
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         
         // Retrieve the verifier address from the environment variable
-        address verifierAddress = vm.envAddress("ZK_REGEX_VERIFIER_ADDRESS");
+        address verifierAddress = vm.envAddress("GROTH16_VERIFIER_ADDRESS");
 
         // Start broadcasting transactions
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy the ProofTypeZKEmail contract
-        ProofTypeZKEmail zkEmailAdapter = new ProofTypeZKEmail(verifierAddress);
+
+        // Start Generation Here
+        ProofTypeGroth16 groth16Adapter = new ProofTypeGroth16(verifierAddress, 7);
+
+        // MyVerifier myVerifier = new MyVerifier(verifierAddress);
+
+        // // Deploy the ProofTypeZKEmail contract
+        // ProofTypeZKEmail zkEmailAdapter = new ProofTypeZKEmail(verifierAddress);
 
         // Stop broadcasting transactions
         vm.stopBroadcast();
 
         // Log the deployed contract address
-        console.log("ProofTypeZKEmail deployed to:", address(zkEmailAdapter));
+        console.log("ProofTypeGroth16 deployed to:", address(groth16Adapter));
     }
 }
 
